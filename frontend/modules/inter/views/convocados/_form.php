81677bc45778e0584d1d670b9ec7952847303155
William Naupa <willymarck@hotmail.com>
2020-08-17 14:57:25 -0500

    Crud convocados

--- frontend/modules/inter/views/convocados/_form.php
+++ frontend/modules/inter/views/convocados/_form.php
@@ -4,6 +4,8 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use frontend\modules\inter\Module as m;
 use yii\helpers\Url;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
 /* @var $this yii\web\View */
 /* @var $model frontend\modules\inter\models\InterConvocados */
 /* @var $form yii\widgets\ActiveForm */
@@ -46,17 +48,62 @@ use yii\helpers\Url;
           
    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
      <p>
-         <?php $url= Url::to(['modal-new-opuniv','id'=>$model->id,'gridName'=>'Grilla1','idModal'=>'buscarvalor']);
+         <?php $url= Url::to(['modal-new-opuniv','id'=>$model->id,'gridName'=>'OpcionesUniversidad','idModal'=>'buscarvalor']);
         //echo  Html::button(yii::t('base.verbs','Modificar Rangos'), ['href' => $url, 'title' => yii::t('sta.labels','Agregar Tutor'),'id'=>'btn_contacts', 'class' => 'botonAbre btn btn-success']); 
        echo Html::a('<span class="btn btn-success btn-sm glyphicon glyphicon-plus"></span>', $url, ['class'=>'botonAbre']);
          ?>           
      </p>
-       
-     <?= Html::button('<span class="fa fa-plus"></span>   '.Yii::t('base_labels', 'Add University'), ['class' => 'btn btn-warning botonAbre'])?>
       
  </div>
      
     <?php ActiveForm::end(); ?>
-
+    <?php Pjax::begin(['id'=>'OpcionesUniversidad']); ?>      
+          
+    <?= GridView::widget([
+        'dataProvider' => new \yii\data\ActiveDataProvider([
+            'query'=> \frontend\modules\inter\models\InterOpuniv::find()->andWhere(['convocatoria_id'=>$model->id])
+        ]),
+        //'summary' => '',
+        'tableOptions'=>['class'=>'table table-condensed table-hover table-bordered table-striped'],
+        //'filterModel' => $searchModel,
+        'columns' => [
+            
+         
+         [
+                'class' => 'yii\grid\ActionColumn',
+                'template' => '{update}{delete}',
+                'buttons' => [
+                    'update' => function($url, $model) {                        
+                        $options = [
+                            'title' => m::t('verbs', 'Update'),                            
+                        ];
+                        $url= Url::to(['modal-edit-opuniv','id'=>$model->id,'gridName'=>'OpcionesUniversidad','idModal'=>'buscarvalor']);
+                        return Html::a('<span class="btn btn-info btn-sm glyphicon glyphicon-pencil"></span>', $url, $options/*$options*/);
+                         },                          
+                         'delete' => function($url, $model) {                        
+                        $options = [
+                            'data-confirm' => m::t('labels', 'Are you sure you want to activate this user?'),
+                            'title' => m::t('verbs', 'Delete'),                            
+                        ];
+                        return Html::a('<span class="btn btn-danger btn-sm glyphicon glyphicon-remove"></span>', $url, $options/*$options*/);
+                         }
+                    ]
+                ],
+         
+         
+         
+         
+         
+            ['attribute'=>'Universidad',
+                'value'=> function($model){
+                    return $model->univop->nombre;
+                }
+                ],                
+            'prioridad',
+            'comentarios',
+        ],
+    ]); ?>
+    <?php Pjax::end(); ?>      
 </div>
+    
     </div>
