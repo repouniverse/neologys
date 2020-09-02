c60c603ccb5ddf76f71d911ba7ba2faa31b5e6a8
William Naupa <willymarck@hotmail.com>
2020-09-02 02:28:10 -0500

    Modificacion Alumnos-Personas

--- frontend/modules/inter/views/programa/_expand_modo_plan.php
+++ frontend/modules/inter/views/programa/_expand_modo_plan.php
@@ -1,141 +1,145 @@
 <?php
-use yii\helpers\Url;
-use yii\helpers\Html;
-use frontend\modules\inter\Module as m;
-use common\widgets\linkajaxgridwidget\linkAjaxGridWidget;
-use common\widgets\buttonajaxwidget\buttonAjaxWidget;
- USE yii\widgets\Pjax;
- //use yii\grid\GridView;
- USE kartik\grid\GridView;
+    use yii\helpers\Url;
+    use yii\helpers\Html;
+    use frontend\modules\inter\Module as m;
+    use common\widgets\linkajaxgridwidget\linkAjaxGridWidget;
+    use common\widgets\buttonajaxwidget\buttonAjaxWidget;
+    use yii\widgets\Pjax;
+    use kartik\grid\GridView;
 ?>
 <?php Pjax::begin(['id'=>'grilla-plan-evaluaciones','timeout'=>false]); ?>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-   
-       <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-          <!-- small box -->
-          <div class="small-box bg-teal-gradient">
+    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+        <div class="small-box bg-teal-gradient">
             <div class="inner">
-                <h3><?php Pjax::begin(['id'=>'pjax-cantidad'.$model->id,'timeout'=>false]); echo $model->getConvocados()->count() ; Pjax::end(); ?></h3>
-
-              <p><?=m::t('labels','Convened Students')?></p>
+                <h3>
+                    <?php 
+                        Pjax::begin(['id'=>'pjax-cantidad'.$model->id,'timeout'=>false]);
+                            echo $model->getConvocados()->count();
+                        Pjax::end();
+                    ?>
+                </h3>
+                <p><?=m::t('labels','Convened Students')?></p>
             </div>
             <div class="icon">
                 <span style="color:white;opacity:0.5;"><i class="fa fa-users"></i></span>
             </div>
             <?php 
-            $url=Url::to(['cantidades-en-riesgo']);
-            echo Html::a(m::t('labels','Convene').'<i class="fa fa-arrow-circle-right"></i>','#', ['id'=>'enlaceconv_'.$model->id,'class'=>"small-box-footer"]);
+                $url=Url::to(['cantidades-en-riesgo']);
+                echo Html::a(m::t('labels','Convene').'<i class="fa fa-arrow-circle-right"></i>','#', ['id'=>'enlaceconv_'.$model->id,'class'=>"small-box-footer"]);
             ?>
             
-          </div>
-          <div class="small-box bg-teal-gradient">
+        </div>
+        <div class="small-box bg-teal-gradient">
             <div class="inner">
                 <h3><?=m::t('labels','Stages')?></h3>
-
-              <p><?=m::t('labels','Stages')?></p>
+                <p><?=m::t('labels','Stages')?></p>
             </div>
             <div class="icon">
                 <span style="color:white;opacity:0.5;"><i class="fa fa-users"></i></span>
             </div>
             <?php 
-            $url=Url::to(['cantidades-en-riesgo']);
-            echo Html::a(m::t('labels','Convene').'<i class="fa fa-arrow-circle-right"></i>','#', ['id'=>'refresh_'.$model->id,'class'=>"small-box-footer"]);
-            ?>
-            
-          </div>
-       
+                $url=Url::to(['cantidades-en-riesgo']);
+                echo Html::a(m::t('labels','Convene').'<i class="fa fa-arrow-circle-right"></i>','#', ['id'=>'refresh_'.$model->id,'class'=>"small-box-footer"]);
+            ?>            
+        </div>
         <?php 
-   echo buttonAjaxWidget::widget([  
-            'id'=>'enlaceconv_'.$model->id,
-            'idGrilla'=>'pjax-cantidad'.$model->id,
-            'ruta'=>Url::to(['/inter/programa/ajax-convoca','id'=>$model->id]),          
-           //'posicion'=> \yii\web\View::POS_END           
-        ]); 
-   ?>
-           <?php 
-   echo buttonAjaxWidget::widget([  
-            'id'=>'refresh_'.$model->id,
-            'idGrilla'=>'pjax-cantidad'.$model->id,
-            'ruta'=>Url::to(['/inter/convocados/ajax-refresh-etapa-exp','id'=>$model->id]),          
-           //'posicion'=> \yii\web\View::POS_END           
-        ]); 
-   ?>
-          
-    
-   </div>
-   <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12"> 
-       <?= GridView::widget([
-        'id'=>'mi-grillhfhfha',
-        'dataProvider' => new \yii\data\ActiveDataProvider(
-                [
-                    'query'=> frontend\modules\inter\models\InterPlan::find()->
-                andWhere(['modo_id'=>$model->id])->orderBy(['etapa_id'=>SORT_ASC,'orden'=>SORT_ASC]),
-                    
-                ]
-                ),
-        //'filterModel' => $searchModel,
-        'columns' => [
-             [
-                'class' => 'yii\grid\ActionColumn',
-                'template' => '{delete}{update}',
-                'buttons' => [
-                   
-                         'update' => function ($url,$model) {
-			    $url= Url::to(['modal-edit-plan','id'=>$model->id,'gridName'=>'grilla-plan-evaluaciones','idModal'=>'buscarvalor']);
-                             //echo  Html::button(m::t('base.verbs','Modificar Rangos'), ['href' => $url, 'title' => m::t('labels','Agregar Tutor'),'id'=>'btn_contacts', 'class' => 'botonAbre btn btn-success']); 
-                            return Html::a('<span class="btn btn-success btn-sm glyphicon glyphicon-pencil"></span>', $url, ['class'=>'botonAbre']);
-                            },
-                          
-                         'delete' => function ($url,$model) {
-			    $url = Url::toRoute($this->context->id.'/ajax-detach-psico',['id'=>$model->id]);
-                             return Html::a('<span class="btn btn-danger btn-sm glyphicon glyphicon-trash"></span>', '#', ['title'=>$url,/*'id'=>$model->codparam,*/'family'=>'holas','id'=> \yii\helpers\Json::encode(['id'=>$model->id,'modelito'=> str_replace('@','\\',get_class($model))]),/*'title' => 'Borrar'*/]);
-                            }
+            echo buttonAjaxWidget::widget
+                 (
+                    [  
+                        'id'=>'enlaceconv_'.$model->id,
+                        'idGrilla'=>'pjax-cantidad'.$model->id,
+                        'ruta'=>Url::to(['/inter/programa/ajax-convoca','id'=>$model->id]),
                     ]
-                ],
-               [
-               'attribute' => 'etapa',
-                   'value'=>function($model){
-                    return $model->etapa_id;    
-                   },
-                    'group'=>TRUE,
-                    ],                     
-                 
-               'orden',
-            'acronimo',
-            'descripcion',
-            //'documento.desdocu'
-            //'parametro',
-            
-            //'valor1',
-            //'valor2',
-
-            
-        ],
-    ]); ?>
-    
-    <?php 
-   echo linkAjaxGridWidget::widget([
-         
-            'idGrilla'=>'grilla-plan-evaluaciones',
-            'family'=>'holas',
-          'type'=>'POST',
-           'evento'=>'click',
-           'posicion'=> \yii\web\View::POS_END
-           
-        ]); 
-   ?>
-    
-
-
-
-  <p>
-         <?php $url= Url::to(['modal-new-plan','id'=>$model->id,'gridName'=>'grilla-plan-evaluaciones','idModal'=>'buscarvalor']);
-                             //echo  Html::button(m::t('base.verbs','Modificar Rangos'), ['href' => $url, 'title' => m::t('labels','Agregar Tutor'),'id'=>'btn_contacts', 'class' => 'botonAbre btn btn-success']); 
-                            echo Html::a('<span class="btn btn-success btn-sm glyphicon glyphicon-plus"></span>', $url, ['class'=>'botonAbre']);
-         ?>           
- </p>
-
-  <?php Pjax::end(); ?>
+                 ); 
+        ?>
+        <?php 
+            echo buttonAjaxWidget::widget
+                 (
+                    [  
+                        'id'=>'refresh_'.$model->id,
+                        'idGrilla'=>'pjax-cantidad'.$model->id,
+                        'ruta'=>Url::to(['/inter/convocados/ajax-refresh-etapa-exp','id'=>$model->id]),
+                    ]
+                 ); 
+        ?>
+    </div>
+    <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12"> 
+        <?= GridView::widget
+            (
+                [
+                    'id'=>'mi-grillhfhfha',
+                    'dataProvider' => new \yii\data\ActiveDataProvider
+                    (
+                        [
+                            'query'=> frontend\modules\inter\models\InterPlan::find()->
+                                      andWhere(['modo_id'=>$model->id])->orderBy(['etapa_id'=>SORT_ASC,'orden'=>SORT_ASC]),
+                        ]
+                    ),
+                    'columns' => 
+                    [
+                        [
+                            'class' => 'yii\grid\ActionColumn',
+                            'template' => '{delete}{update}',
+                            'buttons' => 
+                            [
+                                'update' => function ($url,$model) 
+                                            {
+                                                $url= Url::to
+                                                      (
+                                                        [
+                                                            'modal-edit-plan','id'=>$model->id,'gridName'=>'grilla-plan-evaluaciones',
+                                                            'idModal'=>'buscarvalor'
+                                                        ]
+                                                      );
+                                                return Html::a
+                                                       (
+                                                            '<span class="btn btn-success btn-sm glyphicon glyphicon-pencil"></span>',
+                                                            $url, ['class'=>'botonAbre']
+                                                       );
+                                            },                          
+                                'delete' => function ($url,$model)
+                                            {
+                                                $url = Url::toRoute($this->context->id.'/ajax-detach-psico',['id'=>$model->id]);
+                                                return Html::a
+                                                       (
+                                                            '<span class="btn btn-danger btn-sm glyphicon glyphicon-trash"></span>',
+                                                            '#', ['title'=>$url,'family'=>'holas','id'=> \yii\helpers\Json::encode
+                                                            (['id'=>$model->id,'modelito'=> str_replace('@','\\',get_class($model))]),]);
+                                            }
+                            ]
+                        ],
+                        [
+                            'attribute' => 'etapa',
+                            'value'=>function($model)
+                                     {
+                                        return $model->etapa_id;    
+                                     },
+                            'group'=>TRUE,
+                        ],                 
+                        'orden',
+                        'acronimo',
+                        'descripcion',
+                    ],
+                ]
+            );
+        ?>
+        <?php 
+            echo linkAjaxGridWidget::widget
+            (
+                [         
+                    'idGrilla'=>'grilla-plan-evaluaciones',
+                    'family'=>'holas',
+                    'type'=>'POST',
+                    'evento'=>'click',
+                    'posicion'=> \yii\web\View::POS_END           
+                ]
+            ); 
+        ?>
+        <p>
+            <?php 
+                $url= Url::to(['modal-new-plan','id'=>$model->id,'gridName'=>'grilla-plan-evaluaciones','idModal'=>'buscarvalor']);            
+                echo Html::a('<span class="btn btn-success btn-sm glyphicon glyphicon-plus"></span>', $url, ['class'=>'botonAbre']);
+            ?>           
+        </p>
+        <?php Pjax::end(); ?>
    </div>
-    
--- frontend/modules/inter/views/programa/_modal_eval_plan.php
+++ frontend/modules/inter/views/programa/_modal_eval_plan.php
@@ -1,101 +1,77 @@
 <?php
-
-use common\helpers\h;
-use yii\helpers\Html;
-use yii\widgets\ActiveForm;
-use frontend\modules\inter\helpers\ComboHelper;
-use common\widgets\cbodepwidget\cboDepWidget as ComboDep;
-use backend\modules\base\Module as m;
-use common\widgets\selectwidget\selectWidget;
-
-/* @var $this yii\web\View */
-/* @var $model common\models\masters\Combovalores */
-/* @var $form yii\widgets\ActiveForm */
+    use common\helpers\h;
+    use yii\helpers\Html;
+    use yii\widgets\ActiveForm;
+    use frontend\modules\inter\helpers\ComboHelper;
+    use common\widgets\cbodepwidget\cboDepWidget as ComboDep;
+    use frontend\modules\inter\Module as m;
+    use common\widgets\selectwidget\selectWidget;
 ?>
 <div class="box box-success">
     <div class="box-body">
-<div class="combovalores-form">
-
-   <?php $form = ActiveForm::begin(['id'=>'form-pico',
-        'fieldClass'=>'\common\components\MyActiveField']); ?>
-
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        
-        <div class="col-md-12">
-            <div class="form-group no-margin">
-       <?= \common\widgets\buttonsubmitwidget\buttonSubmitWidget::widget(
-                  ['idModal'=>$idModal,
-                    'idForm'=>'form-pico',
-                      'url'=> ($model->isNewRecord)?\yii\helpers\Url::to(['/inter/programa/modal-new-plan','id'=>$model->modo_id]):
-                     \yii\helpers\Url::to(['/inter/programa/modal-edit-plan','id'=>$model->id]),
-                     'idGrilla'=>$gridName, 
-                      ]
-                  )?>
-               <?=($model->isNewRecord)?'':common\widgets\auditwidget\auditWidget::widget(['model'=>$model])?>
-        
-                
+        <div class="combovalores-form">
+            <?php $form = ActiveForm::begin(['id'=>'form-pico', 'fieldClass'=>'\common\components\MyActiveField']); ?>
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">        
+                <div class="col-md-12">
+                    <div class="form-group no-margin">
+                        <?= \common\widgets\buttonsubmitwidget\buttonSubmitWidget::widget
+                            (
+                                [
+                                    'idModal'=>$idModal,
+                                    'idForm'=>'form-pico',
+                                    'url'=> ($model->isNewRecord)?\yii\helpers\Url::to(['/inter/programa/modal-new-plan','id'=>
+                                             $model->modo_id]):\yii\helpers\Url::to(['/inter/programa/modal-edit-plan','id'=>$model->id]),
+                                    'idGrilla'=>$gridName, 
+                                ]
+                            )
+                        ?>
+                        <?=($model->isNewRecord)?'':common\widgets\auditwidget\auditWidget::widget(['model'=>$model])?>
+                    </div>
+                </div>        
             </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <?= $form->field($model, 'acronimo')->textInput(['maxlength' => true]) ?>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">    
+                <?= $form->field($model, 'descripcion')->textInput(['maxlength' => true]) ?>
+            </div>    
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <?= $form->field($model, 'codocu')->
+                    dropDownList
+                    (
+                        ComboHelper::getCboDocuments() ,
+                        ['prompt'=>'--'.m::t('verbs','Choose a Value')."--",]
+                    )
+                ?>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <?= $form->field($model, 'etapa_id')->
+                    dropDownList
+                    (
+                        ComboHelper::getCboEtapas($model->modo_id),
+                        ['prompt'=>'--'.m::t('verbs','Choose a Value')."--",]
+                    )  
+                ?>
+            </div>   
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <?= $form->field($model, 'eval_id')->
+                    dropDownList
+                    (
+                        ComboHelper::getCboEvaluadores($model->programa_id),
+                        ['prompt'=>'--'.m::t('verbs','Choose a Value')."--",]
+                    )  
+                ?>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <?= $form->field($model, 'orden')->
+                    dropDownList
+                    (
+                        ComboHelper::getCboCardinales(40),
+                        ['prompt'=>'--'.m::t('verbs','Choose a Value')."--",]
+                    )  
+                ?>
+            </div>
+            <?php ActiveForm::end(); ?>
         </div>
-        
-        
-        
-        
-        
-        
     </div>
-   
-     
-    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-    <?= $form->field($model, 'acronimo')->textInput(['maxlength' => true]) ?>
-  </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-    
-    <?= $form->field($model, 'descripcion')->textInput(['maxlength' => true]) ?>
-</div>
-    
-     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <?=$form->field($model, 'codocu')->
-            dropDownList(ComboHelper::getCboDocuments() ,
-                    ['prompt'=>'--'.yii::t('base.verbs','Choose a Value')."--",
-                    // 'class'=>'probandoSelect2',
-                      //'disabled'=>($model->isBlockedField('codpuesto'))?'disabled':null,
-                        ]
-                    )  ?>
-        </div> 
-    
-    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <?=$form->field($model, 'etapa_id')->
-            dropDownList(ComboHelper::getCboEtapas($model->modo_id),
-                    ['prompt'=>'--'.yii::t('base.verbs','Choose a Value')."--",
-                    // 'class'=>'probandoSelect2',
-                      //'disabled'=>($model->isBlockedField('codpuesto'))?'disabled':null,
-                        ]
-                    )  ?>
-        </div>   
-    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <?=$form->field($model, 'eval_id')->
-            dropDownList(ComboHelper::getCboEvaluadores($model->programa_id),
-                    ['prompt'=>'--'.yii::t('base.verbs','Choose a Value')."--",
-                    // 'class'=>'probandoSelect2',
-                      //'disabled'=>($model->isBlockedField('codpuesto'))?'disabled':null,
-                        ]
-                    )  ?>
-        </div>  
-    <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-            <?=$form->field($model, 'orden')->
-            dropDownList(ComboHelper::getCboCardinales(40),
-                    ['prompt'=>'--'.yii::t('base.verbs','Choose a Value')."--",
-                    // 'class'=>'probandoSelect2',
-                      //'disabled'=>($model->isBlockedField('codpuesto'))?'disabled':null,
-                        ]
-                    )  ?>
-        </div>   
-    
-      
-   
-
-    <?php ActiveForm::end(); ?>
-
-</div>
-</div>
 </div>
