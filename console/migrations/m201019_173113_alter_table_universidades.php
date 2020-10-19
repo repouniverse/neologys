debb1f09fdc77113dbf40da98d92724267d67d1a
Julian Ramirez <hipogea@hotmail.com>
2020-10-19 13:20:10 -0500

    ewew

---
+++ console/migrations/m201019_173113_alter_table_universidades.php
@@ -0,0 +1,42 @@
+<?php
+use console\migrations\baseMigration;
+class m201019_173113_alter_table_universidades extends baseMigration
+{
+    const NAME_TABLE='{{%universidades}}';
+    
+    public function safeUp()
+    {
+        $table=static::NAME_TABLE; 
+        if(!$this->existsColumn($table,'web'))
+           $this->alterColumn ($table, 'web', $this->string(20)->append($this->collateColumn())); 
+     
+        
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $table=static::NAME_TABLE;
+        if($this->existsColumn($table,'web'))
+          $this->alterColumn ($table, 'web', $this->string(20)->append($this->collateColumn())); 
+     
+     
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200908_234434_alter_personas cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
