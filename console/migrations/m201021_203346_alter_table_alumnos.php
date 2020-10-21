610ff96e4033d5429e627a4c2abf3198ab58b3ef
Julian Ramirez <hipogea@hotmail.com>
2020-10-21 15:35:44 -0500

    dsds

---
+++ console/migrations/m201021_203346_alter_table_alumnos.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m201021_203346_alter_table_alumnos
+ */
+class m201021_203346_alter_table_alumnos extends Migration
+{
+    const NAME_TABLE='{{%alumnos}}';
+    public function safeUp()
+    {
+        $table=static::NAME_TABLE; 
+        if($this->existsColumn($table,'codalu'))
+           $this->alterColumn ($table, 'codalu', 'varchar(14)'); 
+     
+        
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $table=static::NAME_TABLE;
+        if($this->existsColumn($table,'codalu'))
+          $this->alterColumn ($table, 'codalu', 'varchar(14)'); 
+     
+     
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200908_234434_alter_personas cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
