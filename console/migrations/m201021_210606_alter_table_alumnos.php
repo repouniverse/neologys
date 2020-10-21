97bda2ca5e93ee7ed08cb3552870088c46364c0d
Julian Ramirez <hipogea@hotmail.com>
2020-10-21 16:07:51 -0500

    dsds

---
+++ console/migrations/m201021_210606_alter_table_alumnos.php
@@ -0,0 +1,42 @@
+<?php
+
+use console\migrations\baseMigration;
+class m201021_210606_alter_table_alumnos extends baseMigration
+{
+    const NAME_TABLE='{{%alumnos}}';
+    public function safeUp()
+    {
+        $table=static::NAME_TABLE; 
+        if($this->existsColumn($table,'codfac'))
+           $this->alterColumn ($table, 'codfac', 'varchar(20)'); 
+     
+        
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $table=static::NAME_TABLE;
+        if($this->existsColumn($table,'codfac'))
+          $this->alterColumn ($table, 'codalu', 'varchar(20)'); 
+     
+     
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m200908_234434_alter_personas cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
