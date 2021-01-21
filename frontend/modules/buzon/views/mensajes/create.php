deeb0a9fcbc9a70925c4f623746c37fa1c60f77a
miguel <miguel.ram.dev01@gmail.com>
2021-01-20 22:44:18 -0500

    Buzon / buzon_mensajes / editar_view__buzon_mensajes / proceso / Realizando modificacion de la vista crear y enviar buzon mensaje

--- frontend/modules/buzon/views/mensajes/create.php
+++ frontend/modules/buzon/views/mensajes/create.php
@@ -5,9 +5,13 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\modules\buzon\models\BuzonMensajes */
 
+echo Html::img('http://www.fcctp.usmp.edu.pe/fcctpforms/solicitudbuzon2/images/cabecera_solicitud.jpg',
+[
+    //'align' => 'center',    
+]);
 $this->title = Yii::t('app', 'Create Buzon Mensajes');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Buzon Mensajes'), 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
+$this->params['breadcrumbs'][] = "$this->title";
 ?>
 <div class="buzon-mensajes-create">
 
