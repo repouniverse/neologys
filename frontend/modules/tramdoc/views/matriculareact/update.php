49a6683f5ae86693df0489efe9b6a378467818c6
Jose Luis Huaytalla <joseluishube@gmail.com>
2021-02-09 20:01:01 -0500

    Ajustes de espaciado y texto en vista update

--- frontend/modules/tramdoc/views/matriculareact/update.php
+++ frontend/modules/tramdoc/views/matriculareact/update.php
@@ -19,12 +19,12 @@ $this->params['breadcrumbs'][] = Yii::t('base_labels', 'Update');
 
 
     <div class="box box-body">
-        <div style="width: 90%;">
-            <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12" style="box-shadow: 2px 2px 2px 3px rgba(0, 0, 0, 0.2);">
+        <div style="width: 100%;">
+            <div class="col-lg-5 col-md-6 col-sm-5 col-xs-12" style="box-shadow: 2px 2px 2px 3px rgba(0, 0, 0, 0.1);">
                 <h4><?= h::space(10) . Html::encode("Detalles del Alumno") ?></h4>
-                <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 d-inline" style="word-wrap: break-word;">
+                <div class="col-lg-4 col-md-5 col-sm-5 col-xs-12 d-inline" style="word-wrap: break-word;">
                     <h5><?= Html::label("Modalidad:", null, ["style" => "colordark"]) ?></h5>
-                    <h5><?= Html::label("Pregrado:", null, ["style" => "colordark"]) ?></h5>
+                    <h5><?= Html::label("Periodo:", null, ["style" => "colordark"]) ?></h5>
                     <h5><?= Html::label("Escuela:", null, ["style" => "colordark"]) ?></h5>
                     <h5><?= Html::label("Alumno:", null, ["style" => "colordark"]) ?></h5>
                     <h5><?= Html::label("Dni:", null, ["style" => "color:dark"]) ?></h5>
@@ -33,22 +33,23 @@ $this->params['breadcrumbs'][] = Yii::t('base_labels', 'Update');
                     <h5><?= Html::label("Email Usmp:", null, ["style" => "color:dark"]) ?></h5>
                     <h5><?= Html::label("Email Personal:", null, ["style" => "color:dark"]) ?></h5>
                 </div>
-                <div class="col-lg-7 col-md-7 col-sm-7 col-xs-12 d-inline"style="word-wrap: break-word;">
-                    <h5><?= Html::label("Pre-Grado:", null, ["style" => "color:#565656;font-size: 1.2rem;font-weight: lighter;"]) ?></h5>
-                    <h5><?= Html::label("2021-1", null, ["style" => "color:#565656;font-size: 1.2rem;font-weight: lighter;"]) ?></h5>
-                    <h5><?= Html::label(h::nombreCarrera($model->carrera_id), null, ["style" => "color:#565656;font-size: 1.2rem;font-weight: lighter;"]) ?></h5>
-                    <h5><?= Html::label($model->apellido_paterno . " " . $model->apellido_materno . "," . $model->nombres, null, ["style" => "color:#565656;font-size: 1.2rem;font-weight: lighter;"]) ?></h5>
-                    <h5><?= Html::label($model->dni, null, ["style" => "color:#565656;font-size: 1.2rem;font-weight: lighter;"]) ?></h5>
-                    <h5><?= Html::label($model->celular, null, ["style" => "color:#565656;font-size: 1.2rem;font-weight: lighter;"]) ?></h5>
-                    <h5><?= Html::label($model->telefono, null, ["style" => "color:#565656;font-size: 1.2rem;font-weight: lighter;"]) ?></h5>
-                    <h5><?= Html::label($model->email_usmp, null, ["style" => "color:#565656;font-size: 1.2rem;font-weight: lighter;"]) ?></h5>
-                    <h5><?= Html::label($model->email_personal, null, ["style" => "color:#565656;font-size: 1.2rem;font-weight: lighter;"]) ?></h5>
+
+                <div class="col-lg-8 col-md-7 col-sm-7 col-xs-12 d-inline" style="word-wrap: break-word;">
+                    <h5><?= Html::label("Pre-Grado", null, ["style" => "color:#565656; font-weight:normal"]) ?></h5>
+                    <h5><?= Html::label("2021-1", null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                    <h5><?= Html::label(h::nombreCarrera($model->carrera_id), null, ["style" => "color:#565656; font-weight:normal"]) ?></h5>
+                    <h5><?= Html::label($model->apellido_paterno . " " . $model->apellido_materno . "," . $model->nombres, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                    <h5><?= Html::label($model->dni, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                    <h5><?= Html::label($model->celular, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                    <h5><?= Html::label($model->telefono, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                    <h5><?= Html::label($model->email_usmp, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                    <h5><?= Html::label($model->email_personal, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
                 </div>
             </div>
             <!-- <div class="col-lg-1 col-md-1 col-sm-1 col-xs-12 ">
             </div> -->
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 d-inline">
-                <h4><?= h::space(10) . Html::encode("Actualizar") ?></h4>
+            <div class="col-lg-offset-1 col-lg-6 col-md-6 col-sm-6 col-xs-12 d-inline">
+                <h4><?= h::space(10) . Html::encode("Actualizar Seguimiento") ?></h4>
                 <?= $this->render('_form_update', [
                     'model' => $model,
                     'trabajador' => $trabajador,
