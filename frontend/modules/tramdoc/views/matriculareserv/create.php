79b092961131d2ce2b0a737c2b04a9def3f1d0b6
jorge <jorge.nu.jm@gmail.com>
2021-02-23 12:33:16 -0500

    matricula reserva / en proceso / agregando create

--- frontend/modules/tramdoc/views/matriculareserv/create.php
+++ frontend/modules/tramdoc/views/matriculareserv/create.php
@@ -1,18 +1,19 @@
 <?php
 
 use yii\helpers\Html;
-
+use common\helpers\h;
 /* @var $this yii\web\View */
 /* @var $model frontend\modules\tramdoc\models\TramdocMatriculaReserv */
 
-$this->title = Yii::t('app', 'Create Tramdoc Matricula Reserv');
+$this->title = Yii::t('app', 'CREAR RESERVA DE MATRÍCULA');
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Tramdoc Matricula Reservs'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="tramdoc-matricula-reserv-create">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-
+<h4><?=h::awe('envelope-open-o').h::space(10).Html::encode($this->title) ?></h4>
+<div class="box box-success">
+<br>
     <?= $this->render('_form', [
         'model' => $model,
     ]) ?>
