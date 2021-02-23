c01b63bfb39be5557ce849204bb522d23b2ec62f
miguel <miguel.ram.dev01@gmail.com>
2021-02-23 12:30:09 -0500

    TRAMDOC / CREACION DE CRUD DE RESERVA / RESERVA DE MATRICULAS  / proceso / creando el crud para la reserva de matriculas

--- frontend/modules/tramdoc/views/matriculareserv/update.php
+++ frontend/modules/tramdoc/views/matriculareserv/update.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\helpers\Html;
-
+use common\helpers\h;
 /* @var $this yii\web\View */
 /* @var $model frontend\modules\tramdoc\models\TramdocMatriculaReserv */
 
@@ -14,9 +14,44 @@ $this->params['breadcrumbs'][] = Yii::t('app', 'Update');
 ?>
 <div class="tramdoc-matricula-reserv-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<h4><?= h::awe('pencil') . h::space(10) . Html::encode($this->title) ?></h4>
 
-    <?= $this->render('_form', [
+
+
+<div class="box box-body">
+    <div style="width: 100%;">
+        <div class="col-lg-5 col-md-6 col-sm-5 col-xs-12" style="box-shadow: 2px 2px 2px 3px rgba(0, 0, 0, 0.1);">
+            <h4><?= h::space(10) . Html::encode("Detalles del Alumno") ?></h4>
+            <div class="col-lg-4 col-md-5 col-sm-5 col-xs-12 d-inline" style="word-wrap: break-word;">
+                <h5><?= Html::label("Modalidad:", null, ["style" => "colordark"]) ?></h5>
+                <h5><?= Html::label("Periodo:", null, ["style" => "colordark"]) ?></h5>
+                <h5><?= Html::label("Escuela:", null, ["style" => "colordark"]) ?></h5>
+                <h5><?= Html::label("Alumno:", null, ["style" => "colordark"]) ?></h5>
+                <h5><?= Html::label("Dni:", null, ["style" => "color:dark"]) ?></h5>
+                <h5><?= Html::label("N° Matricula:", null, ["style" => "colordark"]) ?></h5>
+                <h5><?= Html::label("Codigo:", null, ["style" => "colordark"]) ?></h5>
+                <h5><?= Html::label("Celular:", null, ["style" => "color:dark"]) ?></h5>
+                <h5><?= Html::label("Telefono:", null, ["style" => "color:dark"]) ?></h5>
+                <h5><?= Html::label("Email Usmp:", null, ["style" => "color:dark"]) ?></h5>
+                <h5><?= Html::label("Email Personal:", null, ["style" => "color:dark"]) ?></h5>
+            </div>
+
+            <div class="col-lg-8 col-md-7 col-sm-7 col-xs-12 d-inline" style="word-wrap: break-word;">
+                <h5><?= Html::label("Pre-Grado", null, ["style" => "color:#565656; font-weight:normal"]) ?></h5>
+                <h5><?= Html::label("2021-1", null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                <h5><?= Html::label(h::nombreCarrera($model->carrera_id), null, ["style" => "color:#565656; font-weight:normal"]) ?></h5>
+                <h5><?= Html::label($model->apellido_paterno . " " . $model->apellido_materno . "," . $model->nombres, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                <h5><?= Html::label($model->dni, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                <h5><?= Html::label($model->codigo, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                <h5><?= Html::label($model->nro_matr, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                <h5><?= Html::label($model->celular, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                <h5><?= Html::label($model->telefono, null, ["style" => "color:#5 65656;font-weight:normal"]) ?></h5>
+                <h5><?= Html::label($model->email_usmp, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+                <h5><?= Html::label($model->email_personal, null, ["style" => "color:#565656;font-weight:normal"]) ?></h5>
+            </div>
+        </div>
+
+    <?= $this->render('_form_update', [
         'model' => $model,
     ]) ?>
 
