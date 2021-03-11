1b1efe6262f330796356ca0072d1304f7b227e51
lmedinaC <luismal5@hotmail.com>
2021-03-10 22:06:21 -0500

    internacional / agregando menu de internacional para alumno

---
+++ frontend/views/layouts/perfiles/alumno_sin_convocar.php
@@ -0,0 +1,8 @@
+<?php ?>
+
+
+<div>
+
+    <H4>Estimado alumno, no se encuentra convocado para el programa de Internacional.</H4>
+    <p>Comuníquese con el <strong>Departamento de Internacional.</strong></p>
+</div>
