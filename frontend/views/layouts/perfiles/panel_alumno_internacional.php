646f37e14dabe8595afe90b391b9d9f3ac39c06e
Luis Medina <luismal5@hotmail.com>
2021-04-13 10:45:37 -0500

    INTERNACIONAL

--- frontend/views/layouts/perfiles/panel_alumno_internacional.php
+++ frontend/views/layouts/perfiles/panel_alumno_internacional.php
@@ -26,7 +26,7 @@ alumnoAsset::register($this);
             <?php  ?>
         <?php } else {
             $isAdmitido = $convocatoria->isAdmitido();
-            var_dump($convocatoria->universidad->codpais);die();
+            
             $targetUniversidad = $convocatoria->targetUniversity();
             if (is_null($targetUniversidad)) {
                 $univdestino = $convocatoria->universidad;
@@ -38,6 +38,7 @@ alumnoAsset::register($this);
 
                 <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
                     <?php
+                    var_dump($convocatoria->universidad->codpais);die();
                     echo \common\widgets\imagerenderwidget\imageRenderWidget::widget(
                         [
 
